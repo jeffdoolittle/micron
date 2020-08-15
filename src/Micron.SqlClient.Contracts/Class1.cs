63a0e052a2a04c6426215b97d23c93e836ae3eef
Jeff Doolittle <jeff@jeffdoolittle.com>
2020-08-15 06:48:58 -0700

    --wip-- [skip ci]

--- src/Micron.SqlClient.Contracts/Class1.cs
+++ src/Micron.SqlClient.Contracts/Class1.cs
@@ -1,4 +1,13 @@
-﻿using System;
+/* 
+ *  File: Class1.cs
+ *  
+ *  Copyright © 2020 Jeff Doolittle.
+ *  All rights reserved.
+ *  
+ *  Licensed under the BSD 3-Clause License. See LICENSE in project root folder for full license text.
+ */
+
+using System;
 
 namespace Micron.SqlClient.Contracts
 {
--- src/Micron.Tests/UnitTest1.cs
+++ src/Micron.Tests/UnitTest1.cs
@@ -1,3 +1,12 @@
+/* 
+ *  File: UnitTest1.cs
+ *  
+ *  Copyright © 2020 Jeff Doolittle.
+ *  All rights reserved.
+ *  
+ *  Licensed under the BSD 3-Clause License. See LICENSE in project root folder for full license text.
+ */
+
 using System;
 using Xunit;
 
