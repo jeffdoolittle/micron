17f0f0a4be4abcd4c17691b8a552a0009ac4d5f5
Jeff Doolittle <jeff@jeffdoolittle.com>
2020-08-20 01:33:02 -0700

    --wip-- [skip ci]

--- src/Micron.SqlClient/Connect/DbConnectionFactory.cs
+++ src/Micron.SqlClient/Connect/DbConnectionFactory.cs
@@ -1,7 +1,31 @@
 namespace Micron.SqlClient.Connect
 {
-    public class DbConnectionFactory
+    using System;
+    using System.Data.Common;
+
+    public class DbConnectionFactory<TProviderFactory, TBuilder> :
+        IDbConnectionFactory
+        where TProviderFactory : DbProviderFactory
+        where TBuilder : DbConnectionStringBuilder
     {
-        
+        private readonly TProviderFactory providerFactory;
+        private readonly TBuilder builder;
+
+        public DbConnectionFactory(
+            TProviderFactory providerFactory,
+            Action<TBuilder> build)
+        {
+            this.providerFactory = providerFactory;
+            this.builder = (TBuilder)providerFactory.CreateConnectionStringBuilder();
+            build(this.builder);
+        }
+
+        public DbConnection CreateConnection()
+        {
+            var connectionString = this.builder.ConnectionString;
+            var connection = this.providerFactory.CreateConnection();
+            connection.ConnectionString = connectionString;
+            return connection;
+        }
     }
 }
