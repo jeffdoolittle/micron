2d7859fb821fe7cb6ea0590af53819bf0ad0e11a
Jeff Doolittle <jeff@jeffdoolittle.com>
2020-08-25 14:32:44 -0700

    --wip-- [skip ci]

--- src/Micron.TestClient/Program.cs
+++ src/Micron.TestClient/Program.cs
@@ -1,21 +1,74 @@
 ﻿namespace Micron.TestClient
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
 
     public class Program
     {
-        public static int Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            if (args?.Length < 2)
+            using var scope = new ConsoleScope();
+
+            if (args == null || args.Length == 0)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No arguments provided.");
                 return 1;
             }
 
-            var verb = args[0];
+            var command = Commands
+                .FirstOrDefault(_ => _.CanHandle(args)) ?? DefaultCommand;
+
+            await command.HandleAsync(args);
+
+            return 0;
+        }
+
+        private static readonly List<IConsoleCommand> Commands = new List<IConsoleCommand>
+        {
+            new LoadCommand()
+        };
+
+        private static readonly IConsoleCommand DefaultCommand = new HelpCommand();
+    }
+
+    public class HelpCommand : IConsoleCommand
+    {
+        public bool CanHandle(string[] args) => true;
+
+        public Task HandleAsync(string[] args)
+        {
+            Console.WriteLine("Show help information!");
+            return Task.CompletedTask;
+        }
+    }
+
+    public class LoadCommand : IConsoleCommand
+    {
+        public bool CanHandle(string[] args) =>
+            string.Equals("load", args[0], StringComparison.InvariantCultureIgnoreCase);
+
+        public Task HandleAsync(string[] args)
+        {
             var noun = args[1];
 
 
 
+            return Task.CompletedTask;
         }
+    }
+
+    public interface IConsoleCommand
+    {
+        bool CanHandle(string[] args);
+        Task HandleAsync(string[] args);
+    }
+
+    public class ConsoleScope : IDisposable
+    {
+        public void Dispose() =>
+            Console.ResetColor();
     }
 }
