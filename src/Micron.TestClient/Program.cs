b9422533ebda435ffd371a6eac5dc23d190b9596
Jeff Doolittle <jeff@jeffdoolittle.com>
2020-08-25 14:07:14 -0700

    --wip-- [skip ci]

---
+++ src/Micron.TestClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Micron.TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
